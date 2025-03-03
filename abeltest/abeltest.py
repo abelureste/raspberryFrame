8b7ff551b595055c7f42b7fb18e2bc6970713392
abelureste <aureste03@gmail.com>
2025-03-03 15:41:21 -0500

    Updated abeltest

--- abeltest/abeltest.py
+++ abeltest/abeltest.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-import os
+import os, time
 
 from PIL import Image
 from inky.auto import auto
 
-print("""Displaying your images""")
+print("Displaying your images")
 
-PATH = os.path.dirname(__file__)
-
+# Set up inky display
 try:
     inky_display = auto(ask_user=True, verbose=True)
 except NotImplementedError:
@@ -19,6 +18,23 @@ try:
 except NotImplementedError:
     pass
 
+# Retrieve image folder path
+PATH = os.path.dirname(__file__ + "images/...")
+
+while True:
+    try:
+        for filename in PATH:
+            img = Image.open(PATH)
+            img = img.resize(inky_display.resolution)
+
+            inky_display.set_image(img)
+            inky_display.show()
+
+            time.sleep(2 * 60)
+    except Exception as e:
+        print(f"Error")
+
+'''
 if inky_display.resolution == (600, 448):
     img = Image.open(os.path.join(PATH, "images/chewycompressed.jpg"))
     img = img.resize(inky_display.resolution)
@@ -27,3 +43,4 @@ else:
 
 inky_display.set_image(img)
 inky_display.show()
+'''
